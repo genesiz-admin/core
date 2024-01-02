382de577fb0654230a9711d520e3a34d170698d9
LaHiRu <lahirulhr@gmail.com>
2024-01-02 16:36:58 +0530

    simple dependence data transfering

--- src/Domain/UI/Components/FormBuilder/RemoteSelectInput.php
+++ src/Domain/UI/Components/FormBuilder/RemoteSelectInput.php
@@ -2,25 +2,31 @@
 
 namespace Genesizadmin\GenesizCore\Domain\UI\Components\FormBuilder;
 
-class RemoteSelectInput extends FormInput {
+class RemoteSelectInput extends FormInput
+{
 
     protected $component = 'a-remote-select';
 
     public function url($url, $key = 'q')
     {
-        $this->setAttr('url',$url);
-        $this->setAttr('key',$key);
+        $this->setAttr('url', $url);
+        $this->setAttr('key', $key);
 
         return $this;
     }
 
-    public function model($model,$column)
+    public function model($model, $column)
     {
         $url = url("/genz/search-model?column=$column&model=$model");
 
-      $this->url($url);
+        $this->url($url);
 
-      return $this;
+        return $this;
     }
 
+    public function dependOnFields(...$fields)
+    {
+        $this->setAttr('dependents', $fields);
+        return $this;
+    }
 }
